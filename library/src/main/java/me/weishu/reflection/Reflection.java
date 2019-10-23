4396621b7ec70f1e1f6e4fbe0429a5784a6e6ac5
tiann <twsxtd@gmail.com>
2019-10-23 20:58:43 +0800

    Update: don't run when not above P.

--- library/src/main/java/me/weishu/reflection/Reflection.java
+++ library/src/main/java/me/weishu/reflection/Reflection.java
@@ -1,15 +1,12 @@
 package me.weishu.reflection;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.util.Log;
 
 import java.lang.reflect.Method;
 
-import static android.os.Build.VERSION.PREVIEW_SDK_INT;
 import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.P;
 
 /**
  * @author weishu
@@ -23,16 +20,18 @@ public class Reflection {
     private static Method setHiddenApiExemptions;
 
     static {
-        try {
-            Method forName = Class.class.getDeclaredMethod("forName", String.class);
-            Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
+        if (SDK_INT >= Build.VERSION_CODES.P) {
+            try {
+                Method forName = Class.class.getDeclaredMethod("forName", String.class);
+                Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
 
-            Class<?> vmRuntimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
-            Method getRuntime = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null);
-            setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "setHiddenApiExemptions", new Class[]{String[].class});
-            sVmRuntime = getRuntime.invoke(null);
-        } catch (Throwable e) {
-            Log.e(TAG, "reflect bootstrap failed:", e);
+                Class<?> vmRuntimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
+                Method getRuntime = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null);
+                setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "setHiddenApiExemptions", new Class[]{String[].class});
+                sVmRuntime = getRuntime.invoke(null);
+            } catch (Throwable e) {
+                Log.e(TAG, "reflect bootstrap failed:", e);
+            }
         }
 
         // System.loadLibrary("free-reflection");
